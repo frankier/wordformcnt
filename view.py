37eb73d970423c6fb170a989ec3108ebb39f4482
Frankie Robertson <frankie@robertson.name>
2019-12-28 14:04:03 +0200

    Add legends and y limits in view.py

--- view.py
+++ view.py
@@ -1,4 +1,3 @@
-import sys
 import pickle
 import click
 import matplotlib.pyplot as plt
@@ -6,19 +5,22 @@ import matplotlib.pyplot as plt
 
 @click.command()
 @click.argument('inf', type=click.File("rb"))
-@click.argument('outf', type=click.Path())
+@click.argument('outf', type=click.Path(), required=False)
 @click.option('--cmps-only/--all')
 def main(inf, outf, cmps_only):
     df = pickle.load(inf)
     smp = df.iloc[range(-1, -4000, -100)]
     print(smp)
-    if cmps_only:
-        ys = ["cmpspertok"]
-    else:
-        ys = ["cmpspertok", "schspertok", "mwespertok", "allmwespertok"]
+    ys = ["freq", "cmpspertok"]
+    labels = ["Remaining proportion", "Compounds per token"]
+    if not cmps_only:
+        ys += ["schspertok", "mwespertok", "allmwespertok"]
+        labels += ["Schemas per token", "Simple MWEs per token", "Combined MWEs per token"]
+    plt.ylim(0, 1)
     fig = plt.gcf()
     fig.set_size_inches(645.0 / 72, 441.0 / 72)
     df.plot.line(y=ys, logx=True)
+    plt.legend(labels)
     if outf:
         plt.savefig(outf, bbox_inches="tight")
     else:
