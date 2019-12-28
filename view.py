2c0807752fe1bef9f60487a8242d262f5a232144
Frankie Robertson <frankie@robertson.name>
2019-12-28 12:36:13 +0200

    Allow outputting plot to file

--- view.py
+++ view.py
@@ -1,15 +1,28 @@
 import sys
 import pickle
+import click
+import matplotlib.pyplot as plt
 
 
-def main():
-    with open(sys.argv[1], "rb") as inf:
-        df = pickle.load(inf)
+@click.command()
+@click.argument('inf', type=click.File("rb"))
+@click.argument('outf', type=click.Path())
+@click.option('--cmps-only/--all')
+def main(inf, outf, cmps_only):
+    df = pickle.load(inf)
     smp = df.iloc[range(-1, -4000, -100)]
     print(smp)
-    df.plot.line(y=["cmpspertok", "schspertok", "mwespertok", "allmwespertok"])
-    import matplotlib.pyplot as plt
-    plt.show()
+    if cmps_only:
+        ys = ["cmpspertok"]
+    else:
+        ys = ["cmpspertok", "schspertok", "mwespertok", "allmwespertok"]
+    fig = plt.gcf()
+    fig.set_size_inches(645.0 / 72, 441.0 / 72)
+    df.plot.line(y=ys, logx=True)
+    if outf:
+        plt.savefig(outf, bbox_inches="tight")
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
